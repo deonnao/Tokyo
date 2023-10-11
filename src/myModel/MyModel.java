d010a58644e2d221e9164f93e6a408fca3acf777
andreascales <andreascales8@gmail.com>
2023-10-11 12:24:20 -0500

    Adding Supply Constraints

--- src/myModel/MyModel.java
+++ src/myModel/MyModel.java
@@ -2,6 +2,12 @@ package myModel;
 
 public class MyModel {
     public static void main(String[] args) {
-        
+
+        //adding Supply Constraints...
+        for (int i = 0; i < supplyLocations.length; i++) {
+            double[] coefficients = new double[supplyLocations.length * demandLocations.length];
+            coefficients[i * demandLocations.length] = 1;
+            constraints.addConstraint(new LinearConstraint(coefficients, Relationship.LEQ, supplyCapacities[i]));
+        }
     }
 }
